bc8527e3879e689c695e0969051f7e81728312a4
Darwin Froese <froese.darwin@gmail.com>
2015-07-06 12:01:58 -0500

    small refactoring

--- webserv.go
+++ webserv.go
@@ -1,33 +1,28 @@
 package main
 
 import (
+	"NetworkObserver/auth"
 	"html/template"
 	"net/http"
-	"NetworkObserver/auth"
 )
+
+//----------------------------------------------
+// Variables
+//----------------------------------------------
+var previousPage string = ""
 
 func main() {
 	http.HandleFunc("/", root)
 	http.HandleFunc("/checkLogin", checkLogin)
-	http.HandleFunc("/dashboard", dashboard)
+
+	http.HandleFunc("/settings", softwareSettings)
+	http.HandleFunc("/configure", testConfig)
+	http.HandleFunc("/test", currTest)
+	http.HandleFunc("/results", testResults)
 
 	http.ListenAndServe(":8951", nil)
 	// Enable SSL and HTTPS connections
 	//http.ListenAndServeTLS(":8951", "cert.pem", "key.pem", nil)
-}
-
-func root(w http.ResponseWriter, r *http.Request) {
-	servePage(w, r, "html/login.html")
-}
-
-func dashboard(w http.ResponseWriter, r *http.Request) {
-	valid := auth.CheckSessionID(r)
-
-	if valid == true {
-		servePage(w, r, "html/dashboard.html")
-	} else {
-		servePage(w, r, "html/login.html")
-	}
 }
 
 func servePage(w http.ResponseWriter, r *http.Request, pageName string) {
@@ -44,8 +39,76 @@ func checkLogin(w http.ResponseWriter, r *http.Request) {
 
 	if authenticated == true {
 		auth.SetSessionID(w)
-		servePage(w, r, "html/dash-redirect.html")
+		redirect(w, r)
 	} else {
 		servePage(w, r, "html/error.html")
+	}
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	if previousPage == "" {
+		servePage(w, r, "html/dash-redirect.html")
+	} else {
+		servePage(w, r, previousPage)
+	}
+}
+
+func invalidSessionID(callingPage string, w http.ResponseWriter, r *http.Request) {
+	previousPage = callingPage
+	servePage(w, r, "html/login.html")
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	page := "html/dashboard.html"
+	valid := auth.CheckSessionID(r)
+
+	if valid == true {
+		servePage(w, r, page)
+	} else {
+		invalidSessionID(page, w, r)
+	}
+}
+
+func softwareSettings(w http.ResponseWriter, r *http.Request) {
+	page := "html/dashboard/settings.html"
+	valid := auth.CheckSessionID(r)
+
+	if valid == true {
+		servePage(w, r, page)
+	} else {
+		invalidSessionID(page, w, r)
+	}
+}
+
+func testConfig(w http.ResponseWriter, r *http.Request) {
+	page := "html/dashboard/config.html"
+	valid := auth.CheckSessionID(r)
+
+	if valid == true {
+		servePage(w, r, page)
+	} else {
+		invalidSessionID(page, w, r)
+	}
+}
+
+func currTest(w http.ResponseWriter, r *http.Request) {
+	page := "html/dashboard/test.html"
+	valid := auth.CheckSessionID(r)
+
+	if valid == true {
+		servePage(w, r, page)
+	} else {
+		invalidSessionID(page, w, r)
+	}
+}
+
+func testResults(w http.ResponseWriter, r *http.Request) {
+	page := "html/dashboard/results.html"
+	valid := auth.CheckSessionID(r)
+
+	if valid == true {
+		servePage(w, r, page)
+	} else {
+		invalidSessionID(page, w, r)
 	}
 }
