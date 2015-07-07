50562b6ab795a1a894d31923258085ec4b44bf5a
Darwin Froese <froese.darwin@gmail.com>
2015-07-07 14:07:33 -0500

    added handlers for all webpages

--- main.go
+++ main.go
@@ -6,18 +6,16 @@ import (
 )
 
 func main() {
+	// Base Pages
 	http.HandleFunc("/", webserv.Root)
 	http.HandleFunc("/checkLogin", webserv.CheckLogin)
 	http.HandleFunc("/dashboard", webserv.Dashboard)
 
+	// Dashboard Pages
 	http.HandleFunc("/dashboard/settings", webserv.Settings)
-
-	/*
-		TODO: Create handle functions for these
-		"/dashboard/configuration"
-		"/dashboard/current_test"
-		"/dashboard/results"
-	*/
+	http.HandleFunc("/dashboard/configuration", webserv.Configure)
+	http.HandleFunc("/dashboard/current_test", webserv.CurrentTest)
+	http.HandleFunc("/dashboard/results", webserv.Results)
 
 	http.ListenAndServe(":8951", nil)
 	// Enable SSL and HTTPS connections
--- web/webserv.go
+++ web/webserv.go
@@ -41,6 +41,46 @@ func CheckLogin(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func Settings(w http.ResponseWriter, r *http.Request) {
+	valid := auth.CheckSessionID(r)
+
+	if valid == true {
+		servePage(w, r, "htm/dashboard/settings.html")
+	} else {
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
+}
+
+func Configure(w http.ResponseWriter, r *http.Request) {
+	valid := auth.CheckSessionID(r)
+
+	if valid == true {
+		servePage(w, r, "htm/dashboard/config.html")
+	} else {
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
+}
+
+func CurrentTest(w http.ResponseWriter, r *http.Request) {
+	valid := auth.CheckSessionID(r)
+
+	if valid == true {
+		servePage(w, r, "htm/dashboard/test.html")
+	} else {
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
+}
+
+func Results(w http.ResponseWriter, r *http.Request) {
+	valid := auth.CheckSessionID(r)
+
+	if valid == true {
+		servePage(w, r, "htm/dashboard/results.html")
+	} else {
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
+}
+
 func servePage(w http.ResponseWriter, r *http.Request, pageName string) {
 	t, _ := template.ParseFiles(pageName)
 	t.Execute(w, nil)
