cc17a65a664d7b6a7a7885c37d4080ca962f2d7b
Darwin Froese <froese.darwin@gmail.com>
2015-07-22 13:52:41 -0500

    Added sample report xml doc and parser file base

---
+++ xml/parser.go
@@ -0,0 +1,18 @@
+//--------------------------------------------
+// xml/parser.go
+//
+// Reads xml into structs for displaying in
+// webpages.
+// Writes data into xml for storing in xml
+// files to later be accessed.
+//--------------------------------------------
+
+package xml
+
+import (
+	"encoding/xml"
+)
+
+func Read() {
+
+}
