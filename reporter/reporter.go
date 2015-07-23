eca63d21c6036f1c0f2ab6ec54abc56eba13bfee
Darwin Froese <froese.darwin@gmail.com>
2015-07-23 14:39:09 -0500

    cleaned up reporter source

--- reporter/reporter.go
+++ reporter/reporter.go
@@ -1,10 +1,5 @@
 package reporter
 
-/*
-import (
-	"strconv"
-)*/
-
 type Location struct {
 	RoomNumber string `xml:"Location>RoomNumber"`
 	Building   string `xml:"Location>Building"`
@@ -31,25 +26,3 @@ type ReportData struct {
 	SpeedTest
 	Graph string
 }
-
-/*
-func Fill(stringData map[string]string) ReportData {
-	rd := ReportData{}
-
-	rd.Status = "new"
-	rd.RoomNumber = data["room"]
-	rd.Building = data["building"]
-	rd.RunLength = data["run-length"]
-	rd.StartTime = data["start-time"]
-	rd.Uptime = data["uptime"]
-	rd.LastConnect = data["last-connection"]
-	rd.DisconnectCount = Itoa(data["disconnections"])
-	rd.Timeline = data["timeline"]
-	rd.AvgSpeed, _ = ParseFloat(data["average-speed"], 32)
-	rd.MaxSpeed, _ = ParseFloat(data["max-speed"], 32)
-	rd.MinSpeed, _ = ParseFloat(data["min-speed"], 32)
-	// rd.Graph = data["graph"]
-
-	return rd
-}
-*/
