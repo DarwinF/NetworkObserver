69f22401a8403b61a8bc1bbcc49a9ef3e57f3abd
Darwin Froese <froese.darwin@gmail.com>
2016-10-20 19:17:33 -0500

    i#11: restructured the source to be consistent with modern go development

---
+++ cmd/main.go
@@ -0,0 +1,74 @@
+//--------------------------------------------
+// main.go
+//
+// Initializes and sets up Network Observer
+// for use.
+//--------------------------------------------
+
+package main
+
+import (
+	"NetworkObserver/pkg/configuration"
+	logger "NetworkObserver/pkg/logging"
+	settings "NetworkObserver/pkg/settings"
+	websrv "NetworkObserver/pkg/webserver"
+	"net/http"
+	"os"
+)
+
+// Random default value
+var portNumber = "5000"
+var loc = settings.AppLocation
+
+func init() {
+	cl := loc + "/.cookies"
+	logger.WriteString("Removing the existing .cookies file")
+	os.Remove(cl)
+
+	// this is here because apparently auth's init() is called before this one is
+	if _, err := os.Stat(cl); os.IsNotExist(err) {
+		logger.WriteString("Creating a new .cookies file.")
+		file, e := os.Create(cl)
+		if e != nil {
+			logger.WriteString("There was an error creating the .cookies file.")
+		}
+		file.Close()
+	}
+
+	if _, err := os.Stat(loc + "/.password"); os.IsNotExist(err) {
+		logger.WriteString("The .password file does not exist. Creating a new .password file.")
+		file, e := os.Create(loc + "/.password")
+		if e != nil {
+			logger.WriteString("There was an error creating the .password file.")
+		}
+		file.Close()
+	}
+
+	logger.WriteString("Setting the port number to " + configuration.GetPortNumber())
+	portNumber = ":" + configuration.GetPortNumber()
+}
+
+func main() {
+	// Base Pages
+	http.HandleFunc("/", websrv.Root)
+	http.HandleFunc("/checkLogin", websrv.CheckLogin)
+	http.HandleFunc("/dashboard", websrv.Dashboard)
+	http.HandleFunc("/createaccount", websrv.CreateAccount)
+	http.HandleFunc("/account", websrv.HandleAccount)
+
+	// Handlers
+	http.HandleFunc("/saveConfig", websrv.SaveConfig)
+	http.HandleFunc("/savetest", websrv.SaveTest)
+	http.HandleFunc("/teststarted", websrv.TestStarted)
+	http.HandleFunc("/logout", websrv.Logout)
+
+	// Dashboard Pages
+	http.HandleFunc("/dashboard/configure", websrv.Configure)
+	http.HandleFunc("/dashboard/start_test", websrv.StartTest)
+	http.HandleFunc("/dashboard/reports", websrv.Reports)
+
+	// Start the Webserver
+	http.ListenAndServe(portNumber, nil)
+	// Enable SSL and HTTPS connections
+	//http.ListenAndServeTLS(portNumber, "cert.pem", "key.pem", nil)
+}
