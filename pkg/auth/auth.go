b82b525345dda822bfecbbe383f4271a0c234397
Darwin Froese <froese.darwin@gmail.com>
2016-11-15 07:51:44 -0600

    changed return type of authenticator and added init and saving signatures

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -1,7 +1,15 @@
 package auth
 
+func init() {
+
+}
+
+func writeAuthToFile() {
+
+}
+
 // NewAuthenticator returns a new baseAuthenticator
-func NewAuthenticator() *baseAuthenticator {
+func NewAuthenticator() Authenticator {
 
 	return &baseAuthenticator{}
 }
