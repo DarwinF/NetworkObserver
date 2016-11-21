116a52b38839f761498354356eb7dbb631cd35a1
Darwin Froese <froese.darwin@gmail.com>
2016-11-21 00:11:06 -0600

    Wrote package initialization and reading or creating db file.

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -1,31 +1,56 @@
 package auth
 
 import (
+	"os"
+
 	"github.com/darwinfroese/networkobserver/pkg/settings"
 )
 
+var authDb *os.File
+
 func init() {
-	
+	authDbLoc := settings.AuthenticationDBLocation + settings.AuthenticationDBName
+
+	if _, err := os.Stat(authDbLoc); os.IsNotExist(err) {
+		file, err := createFile(authDbLoc)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		authDb = file
+		return
+	}
+
+	file, err := readFile(authDbLoc)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	authDb = file
 }
 
-func readFile(fileLocation string) bool, error {
+func readFile(fileLocation string) (*os.File, error) {
+	file, err := os.Open(fileLocation)
 
+	return file, err
+}
+
+func createFile(fileLocation string) (*os.File, error) {
+	file, err := os.Create(fileLocation)
+
+	return file, err
+}
+
+func writeUserToFile(username, password, salt string) (bool, error) {
 	return false, nil
 }
 
-func createFile(fileLocation string) bool, error {
-
+func updateUsernameInFile(username, newUsername string) (bool, error) {
 	return false, nil
 }
 
-func writeUserToFile(username, password, salt string) bool, error {
-	return false, nil
-}
-
-func updateUsernameInFile(username, newUsername string) bool, error {
-	return false, nil
-}
-
-func updatePasswordInFile(username, password, salt string) bool, error {
+func updatePasswordInFile(username, password, salt string) (bool, error) {
 	return false, nil
 }
